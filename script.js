d9d72a2134f6fd885e194b1fd2590745164f82f2
hlkirkness <hlkirkness@gmail.com>
2021-08-14 15:59:14 -0400

    finalized generator

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ function generatePassword() {
     promptForLength()
   }
 
+  console.log("length: " + length)
+
   var includeLowercase
   var includeUppercase
   var includeNumeric
@@ -30,7 +32,35 @@ function generatePassword() {
     promptForCharacterTypes()
   }
 
-  return "Soon(tm)"
+  console.log("lower?: " + includeLowercase)
+  console.log("upper?: " + includeUppercase)
+  console.log("numeric?: " + includeNumeric)
+  console.log("special?: " + includeSpecial)
+
+  var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  var uppercaseLetters = lowercaseLetters.map(letter => letter.toUpperCase())
+  var numberCharacters = ["1","2","3","4","5","6","7","8","9","0"]
+  var specialCharacters = ["!","@","#","$","%","^","&","*"]
+
+  var selectedCharacters = []
+  if (includeLowercase)
+    selectedCharacters = selectedCharacters.concat(lowercaseLetters)
+  if (includeUppercase)
+    selectedCharacters = selectedCharacters.concat(uppercaseLetters)
+  if (includeNumeric)
+    selectedCharacters = selectedCharacters.concat(numberCharacters)
+  if (includeSpecial)
+    selectedCharacters = selectedCharacters.concat(specialCharacters)
+
+  console.log(selectedCharacters)
+
+  var password = ""
+  for (let i = 0; i < length; i++) {
+    console.log(i)
+    password += selectedCharacters[Math.floor(Math.random() * (selectedCharacters.length - 1))]
+  }
+  console.log(password)
+  return password
 }
 
 // Get references to the #generate element
